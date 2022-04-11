236db04b181dbad7756fb1e6060a73c0c535d137
azeezade <azeezade04@gmail.com>
2022-04-11 20:29:48 +0100

    Made some changes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,3 @@
-const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('./../model/tourModel');
@@ -55,7 +54,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email })._id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
@@ -64,8 +63,6 @@ exports.webhookCheckout = (req, res, next) => {
   let event;
 
   try {
-    const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
-
     event = stripe.webhooks.constructEvent(
       req.body,
       signature,
